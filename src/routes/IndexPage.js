7f0321873442e753cf10ec39473cf9f44c191ff8
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2018-03-18 18:09:09 +0530

    Revert "Revert "Added Products Demo""

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -8,8 +8,9 @@ function IndexPage() {
       <h1 className={styles.title}>Yay! Welcome to React Dva Starter!</h1>
       <div className={styles.welcome} />
       <ul className={styles.list}>
+        <li><a href="/#/products/">Demo</a></li>
         <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
       </ul>
     </div>
   );
