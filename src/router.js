a05783492426f67c55ec3d24b7f40fd9251cc8d4
LIFECOMPILERS\Zilu Rane <zilurrane@gmail.com>
2018-03-18 17:36:23 +0530

    Added Products Demo
    Need to connect mock service

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import Products from './routes/Products';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+        <Route path="/products" exact component={Products} />
       </Switch>
     </Router>
   );
